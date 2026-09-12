e532877c84d9f3bc242308eed864d7c951edb547
Arpit <kushwaharpit18072004@gmail.com>
2026-09-12 21:54:15 +0530

    Time: 4 ms (1.11%), Space: 9.7 MB (72.85%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.c
@@ -0,0 +1,18 @@
+int finalValueAfterOperations(char** operations, int operationsSize) {
+ int x=0;
+    for(int i =0;i<operationsSize;i++){
+        if (strcmp(operations[i], "++X") == 0 || strcmp(operations[i], "X++") == 0)
+        {
+            x+=1;
+
+        }
+        
+        
+       if (strcmp(operations[i], "--X") == 0 || strcmp(operations[i], "X--") == 0){
+            x-=1;
+
+        }
+    }
+    return x;
+    
+}
