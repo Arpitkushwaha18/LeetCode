6107933b8e1f9c89a2568086a40557be4848a63f
Arpit <kushwaharpit18072004@gmail.com>
2026-09-13 15:24:59 +0530

    Time: 0 ms (100%), Space: 9.7 MB (2.29%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.c
@@ -0,0 +1,18 @@
+int mostWordsFound(char** sentences, int sentencesSize) {
+   
+    int max=0;
+    for(int i =0;i<sentencesSize;i++){
+        int m =strlen(sentences[i]);
+         int c =1;
+        for(int j=0;j<m;j++){
+            if(sentences[i][j]==' '){
+                c++;
+            }
+        }
+        if(c>max){
+            max=c;
+        }
+    }
+    return max;
+    
+}
